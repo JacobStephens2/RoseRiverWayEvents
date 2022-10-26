1b0ee03367713d194be88af48229dc1235cacb78
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-10-26 17:56:47 -0400

    add components

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php 
+  $pageTitle = 'Rose River Way Events';
+  include_once('../components/header.php'); 
+?>
+
+<body>
+  <h1>Rose River Way Events</h1>
+</body>
+
+<?php include_once('../components/footer.php'); ?>
+
