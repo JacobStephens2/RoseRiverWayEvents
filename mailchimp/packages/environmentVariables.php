21b3df614e14f40a268bef0d0222b81c48cf1b61
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-11-07 19:03:38 -0500

    Edit files

--- mailchimp/packages/environmentVariables.php
+++
@@ -1,6 +0,0 @@
-<?php 
-define('MAILCHIMP_API_KEY','46814a383d3eedcd41fe043157346257-us19'); 
-define('MAILCHIMP_SERVER_PREFIX','us19'); 
-define('ROSE_RIVER_WAY_WATERCOLOR_LIST_ID','becfdf7b92'); 
-define('ENDPOINT_BASE_URL','https://us19.api.mailchimp.com/3.0/' . ROSE_RIVER_WAY_WATERCOLOR_LIST_ID); 
-?>
